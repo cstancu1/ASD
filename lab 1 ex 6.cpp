31ba29e4b6ed5997017dd5553c0fba83ef1c34a4
Stancu Claudiu Emil <32159498+cstancu1@users.noreply.github.com>
2019-01-05 21:11:58 +0200

    Add files via upload

---
+++ lab 1 ex 6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int x;
+  cout << "Introduceti o valoare pentru x: ";
+  cin >> x;
+  if (x >= 0)
+  {
+      cout << "DA";
+  }
+  else
+  {
+      cout << "NU";
+  }
+  return 0;
+}
